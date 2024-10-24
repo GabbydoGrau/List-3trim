c0a14dfaf8f08306b09d5986ce494ddbc30c9904
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-10-24 20:49:21 -0300

    Ex5.js

--- 3list/Ex5.js
+++ 3list/Ex5.js
@@ -1,5 +1,7 @@
 //algoritmo que lê 3 números...
-let n1 = parseInt(prompt("Digite o 1° número:")); 
+let n1= parseInt(prompt("Digite o 1° número:")); 
 let n2= parseInt(prompt("Digite o 2° número:")); 
 let n3= parseInt(prompt("Digite o 3° número:")); 
 
+let maior= Math.max(n1, n2, n3);
+console.log("O maior é:", maior);

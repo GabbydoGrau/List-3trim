378716f238967544ecdf6d56d0e4fbee1d7e57a2
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-10-24 15:05:51 -0300

    ex2.js

---
+++ 3list/ex2.js
@@ -0,0 +1,14 @@
+// elaborar um algoritimo que lê 3 valores...
+let a = prompt("Digite o valor de a:"); //prompt pra ler os valores 
+let b = prompt("Digite o valor de b:");
+let c = prompt("Digite o valor de c:");
+
+let maior3 = Math.max(a, b, c); //pra encontrar o maior dos 3
+
+let diferenca = Math.abs(a - b); //pra achar a diferença entre a e b
+
+let maiordeAB = Math.max(a, b); //pra achar o maior entre os 2
+
+console.log(`${maior3} é o maior dos 3`);
+console.log(`O maior de a e b é: ${maiorAB}`);
+console.log(`a + b + |a - b| = ${a + b + diferenca}`);

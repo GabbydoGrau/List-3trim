981ca5a153b6f995d0175bdcc594704f36041d93
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-10-24 20:16:40 -0300

    Ex5.js

--- 3list/Ex5.js
+++ 3list/Ex5.js
@@ -1,2 +1,5 @@
 //algoritmo que lê 3 números...
-let n1 = parseInt 
+let n1 = parseInt(prompt("Digite o 1° número:")); 
+let n2= parseInt(prompt("Digite o 2° número:")); 
+let n3= parseInt(prompt("Digite o 3° número:")); 
+

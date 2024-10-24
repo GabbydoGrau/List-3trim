226cea00479d01e138ffd247cc728af36842f9c8
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-10-24 20:10:34 -0300

    ex1.js

--- 3list/ex1.js
+++ 3list/ex1.js
@@ -1,9 +1,9 @@
 // calcular a média e bla bla 
-    const nota1 = parseFloat(prompt("Digite a primeira nota:"));
-    const nota2 = parseFloat(prompt("Digite a segunda nota:"));
-    const nota3 = parseFloat(prompt("Digite a terceira nota:"));
+    const not1 = parseFloat(prompt("Primeira nota:"));
+    const not2 = parseFloat(prompt("Segunda nota:"));
+    const not3 = parseFloat(prompt("Terceira nota:"));
 
-    const media = (nota1 + nota2 + nota3) / 3; //ou pela nota máxima 6?
+    const media = (not1 + not2 + not3) / 3; //ou pela nota máxima 6?
     console.log("A média é: " media);
 
     if (media >= 6) {

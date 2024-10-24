180d57265ae3a468b364598414a05bdd10c984cc
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-10-24 15:16:58 -0300

    ex3.js

---
+++ 3list/ex3.js
@@ -0,0 +1,9 @@
+// algoritmo que lê dois valores...
+let a = prompt("Digite o valor de a:");
+let b = prompt("Digite o valor de b:");
+
+if (a % b === 0 || b % a === 0) { //pra verificar se são multiplos? 
+console.log(${a} e ${b} 'são múltiplos');
+} else {
+console.log(${a} e ${b} 'não são múltiplos'); //eu não consigo imprimir com os valores 
+}

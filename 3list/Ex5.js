ecddf58ccccffd747be28825d5122721ed6d6317
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-10-24 22:27:52 -0300

    Ex5.js

--- 3list/Ex5.js
+++ 3list/Ex5.js
@@ -3,5 +3,9 @@ let n1= parseInt(prompt("Digite o 1° número:"));
 let n2= parseInt(prompt("Digite o 2° número:")); 
 let n3= parseInt(prompt("Digite o 3° número:")); 
 
-let maior= Math.max(n1, n2, n3); //pra não usar tanto if e else 
-console.log("O maior é:", maior);
+if (n1>=n2 && n1>=n3){
+  console.log("O maior é:", n1)
+} if (n2>=n1 && n2>=n3){
+  console.log("O maior é:", n2)
+} else {
+console.log("O maior é:", n3);

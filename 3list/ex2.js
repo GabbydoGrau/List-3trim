9283a0bced65fe9f74e9367c73a717cfd3733ec9
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-10-24 22:31:02 -0300

    ex2.js

--- 3list/ex2.js
+++ 3list/ex2.js
@@ -3,12 +3,12 @@ let a = prompt("Digite o valor de a:"); //prompt pra ler os valores
 let b = prompt("Digite o valor de b:");
 let c = prompt("Digite o valor de c:");
 
-let maior3 = Math.max(a, b, c); //pra encontrar o maior dos 3
+let maior3 = Math.max(a, b, c); //pra encontrar o maior dos 3 sem usar repetição
 
 let diferenca = Math.abs(a - b); //pra achar a diferença entre a e b
 
 let maiordeAB = Math.max(a, b); //pra achar o maior entre os 2
 
-console.log(`${maior3} é o maior dos 3`);
-console.log(`O maior de a e b é: ${maiorAB}`);
-console.log(`a + b + |a - b| = ${a + b + diferenca}`);
+console.log(maior " É o maior dos 3");
+console.log("O maior de a e b é:" maiordeAB);
+console.log("a + b + |a - b| =" a + b + diferenca);

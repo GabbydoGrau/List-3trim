8662bfd575b4e32cf3d0279aac5174d528628544
gabyydograu <163128834+GabbydoGrau@users.noreply.github.com>
2024-10-24 22:31:48 -0300

    ex3.js

--- 3list/ex3.js
+++ 3list/ex3.js
@@ -3,7 +3,7 @@ let a = prompt("Digite o valor de a:");
 let b = prompt("Digite o valor de b:");
 
 if (a % b == 0 || b % a == 0) { //pra verificar se são multiplos 
-console.log('são múltiplos');
+console.log("são múltiplos");
 } else {
-console.log('não são múltiplos'); //eu não consigo imprimir com os valores, não sei como faz :)
+console.log("não são múltiplos"); 
 }
